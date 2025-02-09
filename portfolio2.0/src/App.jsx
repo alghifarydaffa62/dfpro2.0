a712816094a7e26931c04ace7a66385d480edf47
Muhammad Daffa Al Ghifary <daffa.alghifary62@gmail.com>
2025-02-09 10:47:57 +0700

    progress portfolio

--- portfolio2.0/src/App.jsx
+++ portfolio2.0/src/App.jsx
@@ -1,13 +1,11 @@
-import './App.css'
+import './style/App.css'
+import Welcome from './Component/welcome'
 
 function App() {
   return (
-    <>
-      <h1>Vite + React</h1>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Welcome/>
+    </div>
   )
 }
 
---
+++ portfolio2.0/src/Component/welcome.jsx
@@ -0,0 +1,12 @@
+import '../style/App.css';
+
+function Welcome() {
+    return (
+        <div className='Welcome'>
+            <h1>Welcome To My<br/><span className='special'>Portfolio Website</span></h1>
+            <button>Know More About Me</button>
+        </div>
+    )
+}
+
+export default Welcome

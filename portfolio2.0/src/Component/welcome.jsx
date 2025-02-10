a56b92de802e75e73aab11436d60925c426dc698
Muhammad Daffa Al Ghifary <daffa.alghifary62@gmail.com>
2025-02-10 10:09:20 +0700

    AwesomeStyle

--- portfolio2.0/src/Component/welcome.jsx
+++ portfolio2.0/src/Component/welcome.jsx
@@ -3,19 +3,69 @@ import Navbar from './navbar'
 import ethereum from '../Image/coins.png'
 import code from '../Image/code.png'
 import uiux from '../Image/programmer.png'
+import GradientText from '../AwesomeStyle/GradientText/GradientText'
+import AnimatedContent from '../AwesomeStyle/AnimatedContent/AnimatedContent'
 
 function Welcome() {
     return (
         <div>
             <Navbar/>
             <div className='Welcome'>
-                <div className="icon">
-                    <img src={code} alt=""/>
-                    <img src={ethereum} alt=""/>
-                    <img src={uiux} alt=""/>
-                </div>
-                <h1>Welcome To My<br/><span className='special'>Portfolio Website</span></h1>
-                <button>Know More About Me</button>
+                <AnimatedContent
+                    distance={150}
+                    direction="horizontal"
+                    delay={100}
+                    reverse={false}
+                    config={{ tension: 80, friction: 20 }}
+                    initialOpacity={0.2}
+                    animateOpacity
+                    scale={1.1}
+                    threshold={0.2}
+                    >
+                    <div className="icon">
+                        <img src={code} alt=""/>
+                        <img src={ethereum} alt=""/>
+                        <img src={uiux} alt=""/>
+                    </div>
+                </AnimatedContent> 
+                
+                <AnimatedContent
+                    distance={150}
+                    direction="vertical"
+                    delay={100}
+                    reverse={false}
+                    config={{ tension: 80, friction: 20 }}
+                    initialOpacity={0.2}
+                    animateOpacity
+                    scale={1.1}
+                    threshold={0.2}
+                    >
+                    <div><h1>Welcome To My<br/><span className='special'>Portfolio Website</span></h1></div>
+                </AnimatedContent>
+
+                <AnimatedContent
+                    distance={150}
+                    direction="horizontal"
+                    delay={100}
+                    reverse={false}
+                    config={{ tension: 80, friction: 20 }}
+                    initialOpacity={0.2}
+                    animateOpacity
+                    scale={1.1}
+                    threshold={0.2}
+                    >
+                    <div>
+                        <GradientText
+                            colors={["#40ffaa", "#4079ff", "#40ffaa", "#4079ff", "#40ffaa"]}
+                            animationSpeed={3}
+                            showBorder={true}
+                            className="custom-class"
+                        >
+                        Know More About Me
+                        </GradientText>
+                    </div>
+                </AnimatedContent>
+                
             </div>
         </div>
         

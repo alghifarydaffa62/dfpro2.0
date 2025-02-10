dd966c78ccfda31a6cffed4af89ca0c66f24bb86
Muhammad Daffa Al Ghifary <daffa.alghifary62@gmail.com>
2025-02-10 09:31:20 +0700

    Home page

---
+++ portfolio2.0/src/Component/navbar.jsx
@@ -0,0 +1,17 @@
+import '../style/App.css'
+
+function Navbar() {
+    return(
+        <div className='navbar'>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Skills</a></li>
+                <li><a href="">Project</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
--- portfolio2.0/src/Component/welcome.jsx
+++ portfolio2.0/src/Component/welcome.jsx
@@ -1,11 +1,24 @@
-import '../style/App.css';
+import '../style/App.css'
+import Navbar from './navbar'
+import ethereum from '../Image/coins.png'
+import code from '../Image/code.png'
+import uiux from '../Image/programmer.png'
 
 function Welcome() {
     return (
-        <div className='Welcome'>
-            <h1>Welcome To My<br/><span className='special'>Portfolio Website</span></h1>
-            <button>Know More About Me</button>
+        <div>
+            <Navbar/>
+            <div className='Welcome'>
+                <div className="icon">
+                    <img src={code} alt=""/>
+                    <img src={ethereum} alt=""/>
+                    <img src={uiux} alt=""/>
+                </div>
+                <h1>Welcome To My<br/><span className='special'>Portfolio Website</span></h1>
+                <button>Know More About Me</button>
+            </div>
         </div>
+        
     )
 }
 

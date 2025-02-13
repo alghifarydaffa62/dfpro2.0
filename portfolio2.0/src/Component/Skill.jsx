edbb955b027beb9874c2e430858136e7f538680c
Muhammad Daffa Al Ghifary <daffa.alghifary62@gmail.com>
2025-02-13 09:20:11 +0700

    Skill page

--- portfolio2.0/src/Component/Skill.jsx
+++ portfolio2.0/src/Component/Skill.jsx
@@ -1,13 +1,19 @@
 import '../style/App.css'
 import Navbar from './navbar'
+import AnimatedContent from '../AwesomeStyle/AnimatedContent/AnimatedContent'
+import FadeContent from '../AwesomeStyle/FadeContent/FadeContent'
+import Location from './location'
 import reactJS from '../Image/React.png'
 import reactBoostrap from '../Image/React-Bootstrap.png'
 import solidity from '../Image/Solidity.png'
 import tailwind from '../Image/Tailwind-CSS.png'
 import JS from '../Image/JavaScript.png'
 import bootstrap from '../Image/Bootstrap.png'
-import AnimatedContent from '../AwesomeStyle/AnimatedContent/AnimatedContent'
-import FadeContent from '../AwesomeStyle/FadeContent/FadeContent'
+import sertifAI from '../Image/sertifAI.png'
+import sertifFE from '../Image/sertifFrontEnd.png'
+import sertifJS from '../Image/sertifJS.png'
+import sertifReact from '../Image/sertifReact.png'
+import sertifWeb from '../Image/SertifWebPemula.png'
 
 function Skill() {
     const handleAnimationComplete = () => {
@@ -22,6 +28,14 @@ function Skill() {
         JS, 
         bootstrap
     ]
+    const certificate = [
+        sertifAI,
+        sertifFE,
+        sertifJS,
+        sertifReact,
+        sertifWeb,
+    ]
+
     return(
         <div className='Skill'>
             <Navbar/>
@@ -52,6 +66,27 @@ function Skill() {
                 <h2>Certificate</h2>
             </FadeContent>
 
+            <div className="certificate">
+                {certificate.map((sertif) => (
+                    <AnimatedContent
+                        distance={70}
+                        direction="horizontal"
+                        delay={50}
+                        reverse={false}
+                        config={{ tension: 80, friction: 20 }}
+                        initialOpacity={0.2}
+                        animateOpacity
+                        scale={1.1}
+                        threshold={0.2}
+                    >
+                        <div>
+                            <img src={sertif} alt="" />
+                        </div>
+                    </AnimatedContent>
+                ))}
+            </div>
+
+            <Location/>
         </div>
     )
 }

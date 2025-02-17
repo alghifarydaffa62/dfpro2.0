f8a4d5132441d7ccb85f6b39b9c0822abfe83f05
Muhammad Daffa Al Ghifary <daffa.alghifary62@gmail.com>
2025-02-17 20:44:02 +0700

    adding project
    currently busy and no time to code -_-

--- portfolio2.0/src/Component/Project.jsx
+++ portfolio2.0/src/Component/Project.jsx
@@ -4,6 +4,11 @@ import FadeContent from '../AwesomeStyle/FadeContent/FadeContent'
 import code from '../Image/code.png'
 import ethereum from '../Image/coins.png'
 import SpotlightCard from '../AwesomeStyle/SpotlightCard/SpotlightCard'
+import project1 from '../Image/project 1.png'
+import project2 from '../Image/project2.png'
+import project3 from '../Image/project3.png'
+import { Link } from 'react-router-dom'
+import ShinyText from '../AwesomeStyle/ShinyText/ShinyText'
 
 function Project() {
     return(
@@ -27,43 +32,50 @@ function Project() {
                             <h5>Smart Contract</h5>
                         </div>
                     </div>
-                    <div className="box-project">
-                        <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
-                            <h4>Smanisda Cup Aetheria</h4>
-                            <p>An annual event held by the student council of SMA Negeri
-                                1 Sidoarjo. In this project i act as the team leader
-                                and lead the communication with the student council,
-                                web design and code.</p>
-                        </SpotlightCard>
-                        <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
-                            <h4>Mondsia</h4>
-                            <p>An annual event held by the student council of SMA Negeri
-                                1 Sidoarjo. In this project i act as the team leader
-                                and lead the communication with the student council,
-                                web design and code.</p>
-                        </SpotlightCard>
-                        <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
-                            <h4>Todo Apps</h4>
-                            <p>An annual event held by the student council of SMA Negeri
-                                1 Sidoarjo. In this project i act as the team leader
-                                and lead the communication with the student council,
-                                web design and code.</p>
-                        </SpotlightCard>
-                        <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
-                            <h4>Note Apps</h4>
-                            <p>An annual event held by the student council of SMA Negeri
-                                1 Sidoarjo. In this project i act as the team leader
-                                and lead the communication with the student council,
-                                web design and code.</p>
-                        </SpotlightCard>
-                        <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
-                            <h4>Contact Apps</h4>
-                            <p>An annual event held by the student council of SMA Negeri
-                                1 Sidoarjo. In this project i act as the team leader
-                                and lead the communication with the student council,
-                                web design and code.</p>
-                        </SpotlightCard>
-                    </div>
+                    <FadeContent blur={true} duration={1000} easing="ease-out" initialOpacity={0}>
+                        <div className="box-project">
+                            <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
+                                <img src={project1} alt=""/>
+                                <h4>Smanisda Cup Aetheria</h4>
+                                <p>An annual event held by the student council of SMA Negeri
+                                    1 Sidoarjo. In this project i act as the team leader
+                                    and lead the communication with the student council,
+                                    web design and code.</p>
+                                <button><Link>Details</Link></button>
+                            </SpotlightCard>
+                            <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
+                                <img src={project2} alt="" />
+                                <h4>Mondsia</h4>
+                                <p>An annual event held by the student council of SMA Negeri
+                                    1 Sidoarjo. In this project i act as the team leader
+                                    and lead the communication with the student council,
+                                    web design and code.</p>
+                            </SpotlightCard>
+                            <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
+                                <img src={project3} alt="" />
+                                <h4>Life Below Water</h4>
+                                <p>An annual event held by the student council of SMA Negeri
+                                    1 Sidoarjo. In this project i act as the team leader
+                                    and lead the communication with the student council,
+                                    web design and code.</p>
+                            </SpotlightCard>
+                            <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
+                                <h4>Note Apps</h4>
+                                <p>An annual event held by the student council of SMA Negeri
+                                    1 Sidoarjo. In this project i act as the team leader
+                                    and lead the communication with the student council,
+                                    web design and code.</p>
+                            </SpotlightCard>
+                            <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
+                                <h4>Contact Apps</h4>
+                                <p>An annual event held by the student council of SMA Negeri
+                                    1 Sidoarjo. In this project i act as the team leader
+                                    and lead the communication with the student council,
+                                    web design and code.</p>
+                            </SpotlightCard>
+                        </div>
+                    </FadeContent>
+                    
                 </div>
 
 
